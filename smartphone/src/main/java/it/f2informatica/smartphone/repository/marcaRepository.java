576613afb1b6b6220cfc1fc6fced6305853ec9dd
micheleLovero <105788886+micheleLovero@users.noreply.github.com>
2022-06-14 16:25:38 +0200

    Completato:Marca,Os,Smartphone

--- smartphone/src/main/java/it/f2informatica/smartphone/repository/marcaRepository.java
+++ smartphone/src/main/java/it/f2informatica/smartphone/repository/marcaRepository.java
@@ -1,5 +1,32 @@
 package it.f2informatica.smartphone.repository;
 
-public class marcaRepository {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import it.f2informatica.smartphone.model.Marca;
+import it.f2informatica.smartphone.service.marcaService;
+
+
+	@Repository
+	public class marcaRepository implements marcaService {
+
+		@Autowired //crea template
+		JdbcTemplate jdbcTemplate;
+		
+		@Override
+		public List<Marca> findAllGetMarca() {
+			return jdbcTemplate.query("SELECT * FROM marca", BeanPropertyRowMapper.newInstance(Marca.class));
+			
+		}
+
+		@Override
+		public int findPostAllMarca(Marca marca) {
+			return jdbcTemplate.update("INSERT INTO marca (brand , nazionalita) VALUE(?,?)", new Object[] {marca.getBrand(), marca.getNazionalita()});
+		
+			
+		}
 }
--- smartphone/src/main/java/it/f2informatica/smartphone/service/marcaService.java
+++ smartphone/src/main/java/it/f2informatica/smartphone/service/marcaService.java
@@ -1,5 +1,15 @@
 package it.f2informatica.smartphone.service;
 
-public class marcaService {
+import java.util.List;
 
+import it.f2informatica.smartphone.model.Marca;
+
+
+
+public interface marcaService {
+
+
+List<Marca> findAllGetMarca() ;
+	
+	int findPostAllMarca(Marca smartphone);
 }
