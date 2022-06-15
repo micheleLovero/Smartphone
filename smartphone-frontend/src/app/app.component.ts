cc4fd16161d36ed7c15d5f906167bb60146cdadb
micheleLovero <105788886+micheleLovero@users.noreply.github.com>
2022-06-15 10:05:18 +0200

    +angular

--- smartphone-frontend/src/app/app.component.ts
+++ smartphone-frontend/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'smartphone-front-end';
+  title = 'smartphone-frontend';
 }
